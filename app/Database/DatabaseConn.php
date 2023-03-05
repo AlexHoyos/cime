218187e975b38e8ebfa750eab14cdb6855dd1880
Alexandro Hoyos <alexhoyosf080302@gmail.com>
2023-03-05 10:44:58 -0600

    Creación de la conexion a la base de datos y el archivo MAIN (#10)
    * Creación de la base de datos y el archivo MAIN
    
    * Se agrego la conexión de la base de datos al MAIN

---
+++ app/Database/DatabaseConn.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Cime\Database;
+
+class DatabaseConn {
+
+    private \PDO $dbConn;
+
+    public function __construct(
+        private String $engine,
+        private String $host,
+        private Int $port,
+        private String $dbname,
+        private String $user,
+        private String $passw
+    ){ $this->createConn(); }
+
+
+    /**
+     * Funcion que crea la conexión a la base de datos
+     *
+     * @return void
+     */
+    private function createConn():void {
+
+        try {
+
+            $dsn = $this->engine.":host=".$this->host.";dbname=".$this->dbname.";port=".$this->port;
+            $databaseDO = new \PDO($dsn, $this->user, $this->passw);
+
+            $this->dbConn = $databaseDO;
+
+        } catch(\PDOException $e){
+            die("No se pudo conectar a la base de datos: " . $e->getMessage());
+        }
+
+    }
+
+    /**
+     * Función que regresa la conexión creada
+     * 
+     * @return \PDO
+     */
+
+    public function getConnection(): \PDO {
+        return $this->dbConn;
+    }
+
+}
---
+++ app/main.php
@@ -0,0 +1,8 @@
+<?php
+
+use Cime\Database\DatabaseConn;
+
+include_once __DIR__.'./_conf_const.php';
+include_once __DIR__.'./Database/DatabaseConn.php';
+
+$_dbConn = new DatabaseConn(DB_ENGINE, DB_HOST, DB_PORT, DB_DBNAME, DB_USER, DB_PASSW);
