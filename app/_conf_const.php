72d850fba050c10da466f2708740be125c93a1ce
Alexhoyosf <alexhoyosf080302@gmail.com>
2023-03-05 10:29:12 -0600

    Creación de la base de datos y el archivo MAIN

---
+++ app/_conf_const.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+    WEBSITE CONSTANTS
+*/
+
+const WEB_URL = "http://localhost";
+
+/*
+    DATABASE CONSTANTS
+*/
+const DB_ENGINE = "mysql";
+const DB_USER = "root";
+const DB_PASSW = "osopolar1";
+const DB_DBNAME = "cime";
+const DB_HOST = "localhost";
+const DB_PORT = "3306";
---
+++ app/main.php
@@ -0,0 +1,5 @@
+<?php
+
+include_once __DIR__.'./_conf_const.php';
+include_once __DIR__.'./Database/DatabaseConn.php';
+
