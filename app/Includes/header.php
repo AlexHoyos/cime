92a9cdb4127f5c34bace5f3ade010852649c32c1
CarlosCisne019 <99289072+CarlosCisne019@users.noreply.github.com>
2023-03-10 17:56:09 -0600

    Diseño inicial (#15)
    * Header
    
    * Inclusion del header a Index
    
    * Header
    
    * header actualizado
    
    ---------
    
    Co-authored-by: Alexhoyosf <alexhoyosf080302@gmail.com>

---
+++ app/Includes/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?= WEB_URL ?>/assets/css/global.css">
+    <title>CIME</title>
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="<?= WEB_URL ?>/app/Storage/LOGO.png">
+            <input type="text" class="buscar" placeholder="Buscar pelicula...">
+            <button type="button" class="btnbuscar">Buscar</button>
+        </div>
+        <nav>
+            <a href="" class="nav-link-1">Cartelera</a>
+            <a href="" class="nav-link-2">Proximos Estrenos</a>
+            <a href="" class="nav-link-3">Sobre Nosotros</a>
+            <a href="" class="nav-link-4">Contacto</a>
+            <a href="" class="nav-link-5">Iniciar Sesion</a>
+            <a href="" class="nav-link-6">Registro</a>
+        </nav>
+    </header>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
-    include './app/main.php';
+    include __DIR__ . '/app/main.php';
+    include __DIR__ . '/app/Includes/header.php'
 
 ?>
