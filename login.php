73b829038120dc840611d8ffbca1159c29cd2973
Cesar <cirp2411@gmail.com>
2023-03-23 20:11:50 -0600

    acomodando codigo

--- login.php
+++ login.php
@@ -9,54 +9,8 @@
         <div class="col-12" id="pageTitle">
             <h1>Iniciar Sesion</h1>
         </div>
-        <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Iniciar Sesión</title>
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
-    <!-- Custom CSS -->
-    <style>
-        body {
-            background-color: #F8F8F8;
-        }
-        .login-form {
-            width: 340px;
-            margin: 50px auto;
-            font-size: 15px;
-            border: 1px solid #DDD;
-            border-radius: 5px;
-            padding: 20px;
-            background-color: #FFF;
-        }
-        .login-form h2 {
-            margin-bottom: 25px;
-            text-align: center;
-            font-size: 24px;
-        }
-        .form-control, .btn {
-            min-height: 38px;
-            border-radius: 2px;
-        }
-        .btn {        
-            font-size: 15px;
-            font-weight: bold;
-        }
-        .forgot-password {
-            text-decoration: underline;
-        }
-        .logo {
-            display: block;
-            margin: 0 auto;
-            font-size: 48px;
-            text-align: center;
-        }
-    </style>
-</head>
-<body>
+
+
     <div class="login-form">
         <div class="logo">
             <i class="fa-solid fa-key"></i>
@@ -77,10 +31,7 @@
             </div>        
         </form>
     </div>
-    <!-- Bootstrap JS -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</body>
-</html>
+   
 
 
       
