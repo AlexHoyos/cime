c0f6b1544721bf2e7a6254a80f477bd8159eb926
Alexhoyosf <alexhoyosf080302@gmail.com>
2023-04-25 14:43:14 -0600

    CRUD PELIS

--- app/Models/Pelicula.php
+++ app/Models/Pelicula.php
@@ -8,17 +8,23 @@ class Pelicula extends PeliculaDB {
 
     public function getId(){ return $this->id; }
     public function getTitulo(){ return $this->titulo; }
+    public function getAnio() { return $this->anio; }
     public function getSinopsis(){ return $this->sinopsis; }
     public function getPortada(){ return $this->portada; }
     public function getWallpaper(){ return $this->wallpaper; }
     public function getDuracion(){ return $this->duracion; }
     public function getClasificacion(){ return $this->id_clasificacion; }
 
+    public function getClasificacionInstance(): Clasificacion|null{
+        return Clasificacion::getById($this->id_clasificacion);
+    }
+
     public function setTitulo($titulo){ $this->titulo = $titulo; }
+    public function setAnio($anio) { $this->anio = intval($anio) ; }
     public function setSinopsis($sinopsis){ $this->sinopsis = $sinopsis; }
     public function setPortada($portada){ $this->portada = $portada; }
     public function setWallpaper($wallpaper){ $this->wallpaper = $wallpaper; }
-    public function setDuracion($duracion){ $this->duracion = $duracion; }
-    public function setClasificacion($clasificacion){ $this->id_clasificacion = $clasificacion; }
+    public function setDuracion($duracion){ $this->duracion = intval($duracion); }
+    public function setClasificacion($clasificacion){ $this->id_clasificacion = intval($clasificacion); }
 
 }
