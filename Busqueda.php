8acc8d9499bdd3ea8a1c3adbba233c13dfedd3a8
Alexhoyosf <alexhoyosf080302@gmail.com>
2023-05-07 00:50:09 -0600

    Cambios ligeros pagina busqueda

--- Busqueda.php
+++ Busqueda.php
@@ -6,39 +6,46 @@ include './app/Includes/header.php';
 <section class="row m-0 p-0" id="SearchMovie">
     <div class="col-11" id="pageTitle">
         <h1>Busqueda</h1>
-        <h2>Resultados para "FIME"</h2>
+        <h5>Resultados para "FIME"</h5>
     </div>
     <article class="row col-12 px-4">
-        <div class="poster">
-        </div>
-        <div class="info-pelicula1">
-            <h3 id="nompeli">Soy Fimeño</h3>
-            <h5 id="clasificacionpeli">A+</h5>
-            <h5 id="duracionpeli">90 min</h5>
-        </div>
-        <div class="descripcion-pelicula1">
-            <p class="col-12 col-md-6 col-lg-8 text-justify">
-                Soy Fimeño trata de la vida del alumno Luis "Osito" Gutiérrez, un joven de 6to semestre con 7 materias
-                en 5ta,
-                Luis se quiere rendir para cambiarse a FOD pero un día mientras estaba perdido en el edificio 7,
-                se encuentra con el espíritu de Colunga, el cual lo motiva a seguir para que al final descubra
-                que su casa siempre estará en FIME.
-            </p>
-        </div>
-        <div class="container">
-            <div class="star-widget">
-                <input type="radio" name="rate" id="rate-5">
-                <label for="rate-5" class="fas fa-star"></label>
-                <input type="radio" name="rate" id="rate-4">
-                <label for="rate-4" class="fas fa-star"></label>
-                <input type="radio" name="rate" id="rate-3">
-                <label for="rate-3" class="fas fa-star"></label>
-                <input type="radio" name="rate" id="rate-2">
-                <label for="rate-2" class="fas fa-star"></label>
-                <input type="radio" name="rate" id="rate-1">
-                <label for="rate-1" class="fas fa-star"></label>
+        
+
+        <div class="col-12 d-flex flex-row">
+            <div class="d-flex flex-column">
+                <div class="col-12 col-md-6 col-lg-4 div-img m-1"
+                    style="min-height: 350px;background-image: url(/app/Storage/peliculas/poster_1682308028SuperMarioBros6445fbbc9e0b0.jpg);width: 250px;margin-left: 20px;cursor:pointer;">
+                </div>
+                <div>
+                    <div class="login-icon" style="font-size:18px">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fa-regular fa-star"></i>
+                    </div>
+                </div>
+            </div>
+
+            <div class="d-flex flex-column">
+                <h4>Super Mario Bros
+                    <span class="badge bg-dark text-light">A+</span>
+                    <span class="badge bg-dark text-light">90 min</span>
+                </h4>
+                <div class="mx-1" style="max-width:500px">
+                    <p class="col-12 col-md-6 col-lg-8 text-justify">
+                        Soy Fimeño trata de la vida del alumno Luis "Osito" Gutiérrez, un joven de 6to semestre con 7 materias
+                        en 5ta,
+                        Luis se quiere rendir para cambiarse a FOD pero un día mientras estaba perdido en el edificio 7,
+                        se encuentra con el espíritu de Colunga, el cual lo motiva a seguir para que al final descubra
+                        que su casa siempre estará en FIME.
+                    </p>
+                </div>
             </div>
         </div>
+
+        <hr>
+
     </article>
 </section>
 <?php
