bd24d65c5ddda953aed1845ee1c03b9c76aca566
Alexhoyosf <alexhoyosf080302@gmail.com>
2023-05-13 11:57:10 -0600

    Pequeños cambios

--- historialboleto.php
+++ historialboleto.php
@@ -4,84 +4,88 @@ include './app/Includes/header.php';
 
 ?>
 
-<section class="d-flex flex-column" id="histtickets">
+<section class="d-flex flex-column m-0 p-3" id="histtickets">
 
     <div id="pageTitle">
         <h1>Boletos</h1>
         <h3>Pendientes</h3>
     </div>
 
-    <div class="col-6 histboletos1 w-100">
-        <div class="col-12 col-md-6 col-lg-4 div-img"
-            style="min-height: 100px;background-image: url(<?=WEB_URL?>/app/Storage/fimeno.jpg);margin-left: 20px;width: 95px">
+    <div class="d-flex flex-row justify-content-around align-items-center border border-dark w-100">
+        <div class="div-img"
+            style="min-height: 150px;background-image: url(<?=WEB_URL?>/app/Storage/fimeno.jpg);margin-left: 20px;width: 95px">
         </div>
-        <div class="d-flex flex-column mb-2 pendientes1">
+        <div class="d-flex flex-column">
             <p class="p-0"> Pelicula:Pelicula A </p>
             <p class="p-0">Formato: 2D </p>
             <p class="p-0">Idioma:Esp</p>
         </div>
-        <div class="d-flex flex-column mb-2 pendientes2">
+        <div class="d-flex flex-column">
             <p class="p-0"> Dia: 28 Febrero </p>
             <p class="p-0">Hora: 11:10</p>
             <p class="p-0">Sala: 4</p>
         </div>
-        <div class="d-flex flex-column mb-2 pendientes3">
+        <div class="d-flex flex-column">
             <p class="p-0"> Asientos: H1,H2,H3,H4</p>
             <p class="p-0">Adulto: 2</p>
             <p class="p-0">Estudiante: 1</p>
             <p class="p-0">Niño: 1</p>
         </div>
-        <div class="col-12 col-md-6 col-lg-4 div-img"
-            style="min-height: 100px;background-image: url(<?=WEB_URL?>/app/Storage/qrcode.png);margin-left: 1200px;width: 95px;margin-top: -130px">
+        <div class="div-img"
+            style="height: 100px;background-image: url(<?=WEB_URL?>/app/Storage/qrcode.png);width: 95px">
         </div>
     </div>
 
-    <div class="col-6 histboletos2 w-100">
-        <div class="col-12 col-md-6 col-lg-4 div-img"
-            style="min-height: 100px;background-image: url(<?=WEB_URL?>/app/Storage/fimeno.jpg);margin-left: 20px;width: 95px">
+    <div class="d-flex flex-row justify-content-around align-items-center border border-dark w-100">
+        <div class="div-img"
+            style="min-height: 150px;background-image: url(<?=WEB_URL?>/app/Storage/fimeno.jpg);margin-left: 20px;width: 95px">
         </div>
-        <div class="d-flex flex-column mb-2 pendientes1">
-            <p class="p-0"> Pelicula: Pelicula B </p>
+        <div class="d-flex flex-column">
+            <p class="p-0"> Pelicula:Pelicula A </p>
             <p class="p-0">Formato: 2D </p>
             <p class="p-0">Idioma:Esp</p>
         </div>
-        <div class="d-flex flex-column mb-2 pendientes2">
-            <p class="p-0"> Dia: 12 Marzo </p>
-            <p class="p-0">Hora: 16:30</p>
-            <p class="p-0">Sala: 1</p>
+        <div class="d-flex flex-column">
+            <p class="p-0"> Dia: 28 Febrero </p>
+            <p class="p-0">Hora: 11:10</p>
+            <p class="p-0">Sala: 4</p>
         </div>
-        <div class="d-flex flex-column mb-2 pendientes3">
-            <p class="p-0"> Asientos: G4,G5</p>
+        <div class="d-flex flex-column">
+            <p class="p-0"> Asientos: H1,H2,H3,H4</p>
             <p class="p-0">Adulto: 2</p>
+            <p class="p-0">Estudiante: 1</p>
+            <p class="p-0">Niño: 1</p>
         </div>
-        <div class="col-12 col-md-6 col-lg-4 div-img"
-            style="min-height: 100px;background-image: url(<?=WEB_URL?>/app/Storage/qrcode.png);margin-left: 1200px;width: 95px;margin-top: -80px">
+        <div class="div-img"
+            style="height: 100px;background-image: url(<?=WEB_URL?>/app/Storage/qrcode.png);width: 95px">
         </div>
     </div>
 
     <div id="pageTitle">
         <h3>Pasados</h3>
     </div>
-    <div class="col-6 histboletos3 w-100">
-        <div class="col-12 col-md-6 col-lg-4 div-img"
-            style="min-height: 100px;background-image: url(<?=WEB_URL?>/app/Storage/fimeno.jpg);margin-left: 20px;width: 95px">
+    <div class="d-flex flex-row justify-content-around align-items-center border border-dark w-100">
+        <div class="div-img"
+            style="min-height: 150px;background-image: url(<?=WEB_URL?>/app/Storage/fimeno.jpg);margin-left: 20px;width: 95px">
         </div>
-        <div class="d-flex flex-column mb-2 pendientes1">
-            <p class="p-0"> Pelicula: Pelicula C </p>
+        <div class="d-flex flex-column">
+            <p class="p-0"> Pelicula:Pelicula A </p>
             <p class="p-0">Formato: 2D </p>
             <p class="p-0">Idioma:Esp</p>
         </div>
-        <div class="d-flex flex-column mb-2 pendientes2">
-            <p class="p-0"> Dia: 5 Enero </p>
-            <p class="p-0">Hora: 19:00</p>
-            <p class="p-0">Sala: 2</p>
+        <div class="d-flex flex-column">
+            <p class="p-0"> Dia: 28 Febrero </p>
+            <p class="p-0">Hora: 11:10</p>
+            <p class="p-0">Sala: 4</p>
         </div>
-        <div class="d-flex flex-column mb-2 pendientes3">
-            <p class="p-0"> Asientos: H3,H4</p>
+        <div class="d-flex flex-column">
+            <p class="p-0"> Asientos: H1,H2,H3,H4</p>
             <p class="p-0">Adulto: 2</p>
+            <p class="p-0">Estudiante: 1</p>
+            <p class="p-0">Niño: 1</p>
         </div>
-        <div class="col-12 col-md-6 col-lg-4 div-img"
-            style="min-height: 100px;background-image: url(<?=WEB_URL?>/app/Storage/qrcode.png);margin-left: 1200px;width: 95px;margin-top: -50px">
+        <div class="div-img"
+            style="height: 100px;background-image: url(<?=WEB_URL?>/app/Storage/qrcode.png);width: 95px">
         </div>
     </div>
 
