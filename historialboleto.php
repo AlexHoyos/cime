db348272059724dfd670e2161a2dab7b59192e62
CarlosCisne019 <carlitoscisne17@outlook.com>
2023-05-12 21:37:05 -0600

    Modificacion Diseño Historial Boletos

--- historialboleto.php
+++ historialboleto.php
@@ -11,62 +11,82 @@ include './app/Includes/header.php';
         <h3>Pendientes</h3>
     </div>
 
-    <div class="col-6 histboletos1">
+    <div class="col-6 histboletos1 w-100">
         <div class="col-12 col-md-6 col-lg-4 div-img"
-            style="min-height: 100px;background-image: url(fimeno.jpg);margin-left: 20px;width: 95px">
+            style="min-height: 100px;background-image: url(<?=WEB_URL?>/app/Storage/fimeno.jpg);margin-left: 20px;width: 95px">
         </div>
-        <div class="col-4 pendientes">
-            <p class="col-8 col-lg-8 px-4  text-justify" style="margin-left: 100px;margin-top: -100px"> Pelicula:
-                Pelicula A Formato: 2D Idioma:Esp</p>
-            <p class="col-12 col-lg-8 px-4 text-justify" style="margin-left: 220px;margin-top: -115px"> Dia: 28
-                Febrero Hora: 11:10 Sala: 4</p>
-            <p class="col-10  col-lg-8 px-4 text-justify" style="margin-left: 350px;margin-top: -115px"> Asientos:
-                H1,H2,H3,H4 Adulto: 2 Estudiante: 1 Niño: 1</p>
+        <div class="d-flex flex-column mb-2 pendientes1">
+            <p class="p-0"> Pelicula:Pelicula A </p>
+            <p class="p-0">Formato: 2D </p>
+            <p class="p-0">Idioma:Esp</p>
+        </div>
+        <div class="d-flex flex-column mb-2 pendientes2">
+            <p class="p-0"> Dia: 28 Febrero </p>
+            <p class="p-0">Hora: 11:10</p>
+            <p class="p-0">Sala: 4</p>
+        </div>
+        <div class="d-flex flex-column mb-2 pendientes3">
+            <p class="p-0"> Asientos: H1,H2,H3,H4</p>
+            <p class="p-0">Adulto: 2</p>
+            <p class="p-0">Estudiante: 1</p>
+            <p class="p-0">Niño: 1</p>
         </div>
         <div class="col-12 col-md-6 col-lg-4 div-img"
-            style="min-height: 100px;background-image: url(qrcode.png);margin-left: 550px;width: 95px;margin-top: -125px">
+            style="min-height: 100px;background-image: url(<?=WEB_URL?>/app/Storage/qrcode.png);margin-left: 1200px;width: 95px;margin-top: -130px">
         </div>
     </div>
 
-    <div class="col-6 histboletos2">
+    <div class="col-6 histboletos2 w-100">
         <div class="col-12 col-md-6 col-lg-4 div-img"
-            style="min-height: 100px;background-image: url(fimeno.jpg);margin-left: 20px;width: 95px;margin-top: 50px">
+            style="min-height: 100px;background-image: url(<?=WEB_URL?>/app/Storage/fimeno.jpg);margin-left: 20px;width: 95px">
         </div>
-        <div class="col-4 pendientes">
-            <p class="col-8 col-lg-8 px-4  text-justify" style="margin-left: 100px;margin-top: -100px"> Pelicula:
-                Pelicula B Formato: 2D Idioma:Esp</p>
-            <p class="col-12 col-lg-8 px-4 text-justify" style="margin-left: 220px;margin-top: -115px"> Dia: 12
-                Marzo Hora: 16:30 Sala: 1</p>
-            <p class="col-10  col-lg-8 px-4 text-justify" style="margin-left: 350px;margin-top: -90px"> Asientos:
-                G4,G5 Adulto: 2</p>
+        <div class="d-flex flex-column mb-2 pendientes1">
+            <p class="p-0"> Pelicula: Pelicula B </p>
+            <p class="p-0">Formato: 2D </p>
+            <p class="p-0">Idioma:Esp</p>
+        </div>
+        <div class="d-flex flex-column mb-2 pendientes2">
+            <p class="p-0"> Dia: 12 Marzo </p>
+            <p class="p-0">Hora: 16:30</p>
+            <p class="p-0">Sala: 1</p>
+        </div>
+        <div class="d-flex flex-column mb-2 pendientes3">
+            <p class="p-0"> Asientos: G4,G5</p>
+            <p class="p-0">Adulto: 2</p>
         </div>
         <div class="col-12 col-md-6 col-lg-4 div-img"
-            style="min-height: 100px;background-image: url(qrcode.png);margin-left: 550px;width: 95px;margin-top: -80px">
+            style="min-height: 100px;background-image: url(<?=WEB_URL?>/app/Storage/qrcode.png);margin-left: 1200px;width: 95px;margin-top: -80px">
         </div>
     </div>
 
     <div id="pageTitle">
         <h3>Pasados</h3>
     </div>
-
-    <div class="col-6 histboletos3">
+    <div class="col-6 histboletos3 w-100">
         <div class="col-12 col-md-6 col-lg-4 div-img"
-            style="min-height: 100px;background-image: url(fimeno.jpg);margin-left: 20px;width: 95px;margin-top: 5px">
+            style="min-height: 100px;background-image: url(<?=WEB_URL?>/app/Storage/fimeno.jpg);margin-left: 20px;width: 95px">
         </div>
-        <div class="col-4 pasados">
-            <p class="col-8 col-lg-8 px-4  text-justify" style="margin-left: 100px;margin-top: -110px"> Pelicula:
-                Pelicula C Formato: 2D Idioma:Esp</p>
-            <p class="col-12 col-lg-8 px-4 text-justify" style="margin-left: 220px;margin-top: -115px"> Dia: 05
-                Enero Hora: 19:00 Sala: 2</p>
-            <p class="col-10  col-lg-8 px-4 text-justify" style="margin-left: 350px;margin-top: -90px"> Asientos:
-                H3,H4 Adulto: 2</p>
+        <div class="d-flex flex-column mb-2 pendientes1">
+            <p class="p-0"> Pelicula: Pelicula C </p>
+            <p class="p-0">Formato: 2D </p>
+            <p class="p-0">Idioma:Esp</p>
+        </div>
+        <div class="d-flex flex-column mb-2 pendientes2">
+            <p class="p-0"> Dia: 5 Enero </p>
+            <p class="p-0">Hora: 19:00</p>
+            <p class="p-0">Sala: 2</p>
+        </div>
+        <div class="d-flex flex-column mb-2 pendientes3">
+            <p class="p-0"> Asientos: H3,H4</p>
+            <p class="p-0">Adulto: 2</p>
         </div>
         <div class="col-12 col-md-6 col-lg-4 div-img"
-            style="min-height: 100px;background-image: url(qrcode.png);margin-left: 550px;width: 95px;margin-top: -60px;margin-bottom: 10px">
+            style="min-height: 100px;background-image: url(<?=WEB_URL?>/app/Storage/qrcode.png);margin-left: 1200px;width: 95px;margin-top: -50px">
         </div>
     </div>
 
 </section>
+
 
 <?php
 include './app/Includes/footer.php';
