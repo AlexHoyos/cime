adb8521642d4b99c5a085ce3af1258c27cd87ee6
CesarIvanRodriguezPerez <123857366+CesarIvanRodriguezPerez@users.noreply.github.com>
2023-05-26 20:36:04 -0600

    LECTOR QR EN ADMIN (#103)
    * LIBRERIA DE LECTOR DE QR ACTUALIZADA
    
    Co-authored-by: Alexandro Hoyos <alexhoyosf080302@gmail.com>

--- admin/escaner.php
+++ admin/escaner.php
@@ -42,50 +42,40 @@ include '../app/Includes/Admin/Dashboard.php';
 <body>
     <div id="mainContainer">
         <h1>Escáner de código QR</h1>
-        <div id="camaraContainer"></div>
+        <video id="camaraContainer"></video>
         <div id="botonesContainer">
             <input type="text" id="codigoManual" class="form-control" placeholder="Ingrese el código manualmente">
             <button onclick="leerCodigoManual()" class="btn btn-primary">Leer código manual</button>
         </div>
     </div>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/quagga/0.12.1/quagga.min.js"></script>
     <script>
-        // Función para iniciar el escaneo del código QR
-        function iniciarEscaneo() {
-            Quagga.init({
-                inputStream: {
-                    name: "Live",
-                    type: "LiveStream",
-                    target: document.querySelector("#camaraContainer")
-                },
-                decoder: {
-                    readers: ["code_128_reader", "ean_reader", "ean_8_reader", "code_39_reader", "code_39_vin_reader", "codabar_reader", "upc_reader", "upc_e_reader", "i2of5_reader"]
-                }
-            }, function(err) {
-                if (err) {
-                    console.error(err);
-                    return;
-                }
-                Quagga.start();
-            });
-
-            Quagga.onDetected(function(result) {
-                var codigo = result.codeResult.code;
-                alert("Código QR detectado: " + codigo);
-            });
+    // Función para leer el código manualmente
+    function leerCodigoManual() {
+        var codigoManual = document.getElementById("codigoManual").value;
+        if (codigoManual !== "") {
+            alert("Código ingresado manualmente: " + codigoManual);
         }
+    }
+    </script>
 
-        // Función para leer el código manualmente
-        function leerCodigoManual() {
-            var codigoManual = document.getElementById("codigoManual").value;
-            if (codigoManual !== "") {
-                alert("Código ingresado manualmente: " + codigoManual);
-            }
-        }
+    <script type="module">
+        import QrScanner from "../assets/js/qrscanner.js";
+       // Scanner Object
+       const scanner = new QrScanner(
+            document.getElementById("camaraContainer"), 
+            function(result){
+                //document.getElementById("scanresult").value = result;
+                alert(result)
+            } 
+        );
+       
+       //document.getElementById("start").onclick = e => scanner.start();
+       //document.getElementById("stop").onclick = e => scanner.stop();
+
 
         // Iniciar el escaneo al cargar la página
-        window.onload = iniciarEscaneo;
+        window.onload = e => scanner.start();
     </script>
 </html>
     <?php
